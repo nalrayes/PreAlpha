a66cfa8b182a13ee622bdcdf322bcbdb6e476617
nalrayes <na1487@nyu.edu>
2017-02-01 18:03:29 -0500

    added door switch script, working on posession mechanic

---
+++ Assets/SwitchScripy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScripy : MonoBehaviour {
+
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
+		door.SetActive (false);	
+		
+		this.GetComponent<SpriteRenderer> ().color = new Color32 (67, 161, 99, 255);	
+	}
+
+}
