9da0a289c6cb266b7b23fd48af287550e6cec119
nalrayes <na1487@nyu.edu>
2017-02-02 14:48:25 -0500

    posession prototype working!

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -5,15 +5,48 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour {
 
 	bool posessed = false;
+	float timer;
+	float posessionSpeed;
+
+	GameObject jinn;
+
 
 	// Use this for initialization
 	void Start () {
-		this.tag = "enemy";
-		
+		timer = 0;
+		posessionSpeed = 2.0f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (posessed) {
+			timer++;
+			if (Input.GetKey(KeyCode.RightArrow)){
+				//lastDirection = KeyCode.RightArrow;
+				this.transform.position += Vector3.right * posessionSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow)){
+				//lastDirection = KeyCode.LeftArrow;
+				this.gameObject.transform.position += Vector3.left* posessionSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.UpArrow)){
+				//lastDirection = KeyCode.UpArrow;
+				this.gameObject.transform.position += Vector3.up * posessionSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.DownArrow)){
+				//lastDirection = KeyCode.DownArrow;
+				this.gameObject.transform.position += Vector3.down * posessionSpeed * Time.deltaTime;
+			}
+
+			if (timer > 150) {
+				timer = 0;
+				posessed = false;
+				jinn.SetActive (true);
+				Vector3 rightOf = new Vector3 (1.2f, 0f);
+				jinn.transform.position = this.transform.position + rightOf;
+				jinn.transform.rotation = this.transform.rotation;
+			}
+		}
 		
 	}
 
@@ -21,6 +54,11 @@ public class EnemyScript : MonoBehaviour {
 		if (collisionInfo.gameObject.CompareTag("jinn")) {
 			posessed = true;
 			this.GetComponent<SpriteRenderer> ().color = Color.gray;
+			jinn = collisionInfo.gameObject;
+
+			jinn.SetActive (false);
+			timer = 0;
+
 		}
 	}
 }
