7f46adce84b773b79ab26e15a4d7da7c16d01787
nalrayes <na1487@nyu.edu>
2017-02-15 17:33:33 -0500

    move towards works! need to implement attacks, also stop rotations

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -6,27 +6,36 @@ public class EnemyAI : MonoBehaviour {
 
 	bool playerVisible;
 	Vector3 originalPosition;
+
+	float speed = 5f;
 	// Use this for initialization
+
+	GameObject summoner;
 	void Start () {
+		summoner = GameObject.FindGameObjectWithTag ("summoner");
 		playerVisible = false;
 		originalPosition = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (playerVisible) {
-			this.moveToPlayer ();
-		} else {
-			//look for player, if within range set true
+		Debug.Log(Vector3.Distance (summoner.transform.position, transform.position));
+//		moveToPlayer ();
+		if (Vector3.Distance (summoner.transform.position, transform.position) <= 10f) {
+			moveToPlayer ();
 		}
 	}
-
-	void moveToPlayer() {
-		/*
-		 * move to player vector position
-		 * if in attack range stop and attack at player vector position
-		 * look for player, if not found return to original position
-		 */
+	/*
+	 * move to player vector position
+	 * if in attack range stop and attack at player vector position
+	 * look for player, if not found return to original position
+	 */
+	void moveToPlayer () {
+		transform.position = Vector3.MoveTowards (transform.position, summoner.transform.position, 2*Time.deltaTime);
+		// if within range, attack
+		if (Vector3.Distance (summoner.transform.position, transform.position) > 2) {
+			//attack
+		}
 	}
 
 	void moveTo(Vector3 location) {
