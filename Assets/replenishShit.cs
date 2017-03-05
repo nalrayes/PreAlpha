c4c1254e82b106a84668c5e164e7095d234020ca
nalrayes <na1487@nyu.edu>
2017-03-05 18:27:32 -0500

    shitty version of walls works, NEED to find and fix space bug for switches. level 4 is spikes, coming up next

---
+++ Assets/replenishShit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class replenishShit : MonoBehaviour {
+	int timer;
+	// Use this for initialization
+	void Start () {
+		timer = 500;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer == 500) {
+			//keep at 500
+		} else {
+			timer++;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D info) {
+		if (timer == 500) {
+			if (info.gameObject.CompareTag ("summoner")) {
+				info.gameObject.GetComponent<PropertyScript> ().changeMana (5);
+				info.gameObject.GetComponent<PropertyScript> ().changeHealth (10);
+				timer = 0;
+			}
+		}
+	}
+}
