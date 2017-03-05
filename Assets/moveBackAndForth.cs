58d4c1ccae2585023f46602f15ef44ad62ad5a8c
nalrayes <na1487@nyu.edu>
2017-03-05 18:48:56 -0500

    started level 4, working out spike scirpt

---
+++ Assets/moveBackAndForth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBackAndForth : MonoBehaviour {
+	public Vector3 direction;
+	Vector3 originalPos;
+
+	// Use this for initialization
+	void Start () {
+		originalPos = transform.position;
+		direction = Vector3.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (transform.position.y == -1 * originalPos.y);
+		if (transform.position.y == -1 * originalPos.y) {
+			direction = -1 * direction;
+		}
+		gameObject.transform.position += 5f* direction * Time.deltaTime;
+
+	}
+}
